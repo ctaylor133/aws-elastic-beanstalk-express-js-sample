fb990b91f8e0f364c9a502063bc0ec1f6e940f7c
ctaylor133 <44619551+ctaylor133@users.noreply.github.com>
2021-03-08 15:14:12 -0800

    Update app.js
    added VERY

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('stinky-butt!'));
+app.get('/', (req, res) => res.send('VERY stinky-butt!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
